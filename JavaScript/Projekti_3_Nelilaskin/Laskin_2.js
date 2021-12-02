90c0939044233260ec835cf022f80b1c2665a93d
JyriTurkkila <jyri.turkkila@edu.keuda.fi>
2021-12-02 09:58:29 +0200

    Nelilaskin 2.12.2021

--- JavaScript/Projekti_3_Nelilaskin/Laskin_2.js
+++ JavaScript/Projekti_3_Nelilaskin/Laskin_2.js
@@ -1,15 +1,14 @@
-
 // Inputin arvon määritys
-function inputFieldDiv() { const inputField = document.getElementById('input').innerHTML; }
+const inputField = document.getElementById('input');
 
 // Lisää merkin inputtiin
-function insertNumDiv() { const insertNum = num => inputField.textContent += num; }
+const insertNum = num => inputField.textContent += num;
 
 // Suorittaa laskennan
-function equalToDiv() { equalTo = () => (inputField.textContent) ? inputField.textContent = eval(inputField.textContent) : false; }
+const equalTo = () => (inputField.textContent) ? inputField.textContent = eval(inputField.textContent) : false;
 
 // Poistaa yhden merkin inputista
-function eraseNumDiv() { const eraseNum = () => inputField.textContent = inputField.textContent.substring(0, inputField.textContent.length - 1); }
+const eraseNum = () => inputField.textContent = inputField.textContent.substring(0, inputField.textContent.length - 1);
 
 // Tyhjentää inputin
-function clearInputDiv() { const clearInput = () => inputField.textContent = ''; }
+const clearInput = () => inputField.textContent = '';
